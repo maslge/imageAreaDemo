2d1152290171569f284a95f88a3aceded6d84828
Bruno DARRAS <bruno.darras@gmail.com>
2020-04-07 21:52:19 +0200

    final release

--- src/deploy.sh
+++ src/deploy.sh
@@ -2,8 +2,7 @@
 
 #from https://www.techrepublic.com/article/how-to-easily-add-an-ssh-fingerprint-to-your-knownhosts-file-in-linux/
 #ssh-keyscan -H 192.168.1.162 >> ~/.ssh/known_hosts
-
 scp -o "StrictHostKeyChecking=no" ./build/dofin.tgz bitnami@18.197.40.62:/opt/bitnami/nginx/html/
-#ssh -o "StrictHostKeyChecking=no" bitnami@18.197.40.62  < ./src/deploy.sh;
-#cd /opt/bitnami/nginx/html/
-#tar -xzvf dofin.tgz -C ./dofin/
+
+#from https://unix.stackexchange.com/questions/349425/ssh-command-and-non-interactive-non-login-shell
+ssh -o "StrictHostKeyChecking=no" bitnami@18.197.40.62  < ./src/deployOnRemoteHost.sh;
