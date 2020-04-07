7a4ee518d7ad4baad79962c7f5e746bf6b4a36da
Bruno DARRAS <bruno.darras@gmail.com>
2020-04-07 21:34:42 +0200

    fix issue

--- src/deploy.sh
+++ src/deploy.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
-cd /opt/bitnami/nginx/html/
-tar -xzvf dofin.tgz -C ./dofin/
+#from https://www.techrepublic.com/article/how-to-easily-add-an-ssh-fingerprint-to-your-knownhosts-file-in-linux/
+#ssh-keyscan -H 192.168.1.162 >> ~/.ssh/known_hosts
+
+scp -o "StrictHostKeyChecking=no" ./build/dofin.tgz bitnami@18.197.40.62:/opt/bitnami/nginx/html/
+#ssh -o "StrictHostKeyChecking=no" bitnami@18.197.40.62  < ./src/deploy.sh;
+#cd /opt/bitnami/nginx/html/
+#tar -xzvf dofin.tgz -C ./dofin/
