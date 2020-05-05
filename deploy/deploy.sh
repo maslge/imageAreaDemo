8976314f9b3cc1589531bcf4f040c3982297882f
Bruno DARRAS <bruno.darras@gmail.com>
2020-05-05 21:09:49 +0200

    add csv support

---
+++ deploy/deploy.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+#from https://www.techrepublic.com/article/how-to-easily-add-an-ssh-fingerprint-to-your-knownhosts-file-in-linux/
+#ssh-keyscan -H 192.168.1.162 >> ~/.ssh/known_hosts
+scp -o "StrictHostKeyChecking=no" ./build/dofin.tgz bitnami@18.197.40.62:/opt/bitnami/nginx/html/
+
+#from https://unix.stackexchange.com/questions/349425/ssh-command-and-non-interactive-non-login-shell
+ssh -o "StrictHostKeyChecking=no" bitnami@18.197.40.62  < ./deploy/deployOnRemoteHost.sh;
